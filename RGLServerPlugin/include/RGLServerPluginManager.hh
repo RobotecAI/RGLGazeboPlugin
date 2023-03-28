dba196a232aa4e64f1cdf0592afd58e3af807c1d
Mateusz Szczygielski <mateusz.szczygielski@robotec.ai>
2023-03-28 17:26:35 +0200

    Remove leftover macro

--- RGLServerPlugin/include/RGLServerPluginManager.hh
+++ RGLServerPlugin/include/RGLServerPluginManager.hh
@@ -26,16 +26,6 @@
 
 #include <ignition/transport/Node.hh>
 
-#define RGL_CHECK(call)              \
-do {                                 \
-    rgl_status_t status = call;      \
-    if (status != RGL_SUCCESS) {     \
-    const char* msg;                 \
-    rgl_get_last_error_string(&msg); \
-    ignerr << msg << "\n";           \
-    }                                \
-} while(0)
-
 namespace rgl
 {
 
