c417dc77204fe60107f21f0ceddacb64e8eb06a0
Jacob Seibert <jacob.seibert@enway.ai>
2024-11-06 13:39:09 +0100

    Add Livox Horizon preset

--- RGLServerPlugin/src/LidarPatternLoader.cc
+++ RGLServerPlugin/src/LidarPatternLoader.cc
@@ -36,6 +36,7 @@ std::map<std::string, std::pair<std::string, std::size_t>> LidarPatternLoader::p
     {"Pandar64", {"HesaiPandarQT64.mat3x4f", 0}},
     {"Pandar40P", {"HesaiPandar40P.mat3x4f", 0}},
     {"Livox Avia", {"LivoxAvia.mat3x4f", 24000}},
+    {"Livox Horizon", {"LivoxHorizon.mat3x4f", 24000}},
     {"Livox Mid360", {"LivoxMid360.mat3x4f", 20000}},
 };
 
